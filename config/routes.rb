2b04836a0e175db3b11629049e4029f13977aa12
chiu <chiucode@gmail.com>
2015-10-05 08:13:19 +0000

    added controllers for project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/edit'
+
   root to: 'visitors#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # devise_for :users
