4a8b7f1b0de96529cc7a935517dab52cecd31b8e
chiu <chiucode@gmail.com>
2015-10-05 10:08:39 +0000

    finally works; rmoved session stuff from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,9 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation) }
     end
 
-    def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+    # def current_user
+    #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # end
 
-    helper_method :current_user
+    # helper_method :current_user
 end
