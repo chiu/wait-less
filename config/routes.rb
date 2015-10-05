9bd04c65687f1725fb9d043e37adaac4b2766883
chiu <chiucode@gmail.com>
2015-10-05 08:21:45 +0000

    finished project forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  get 'projects/index'
+  # get 'projects/index'
 
-  get 'projects/show'
+  # get 'projects/show'
 
-  get 'projects/new'
+  # get 'projects/new'
 
-  get 'projects/edit'
+  # get 'projects/edit'
+
+
 
   root to: 'visitors#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # devise_for :users
 
   resources :users
+
+  resources :projects
 end
