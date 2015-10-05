8e50a18b60649e9ed27bc475fae1bffdeaee842c
chiu <chiucode@gmail.com>
2015-10-05 11:38:10 +0000

    added vaidations to pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,23 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+
+  
+  validates :user,
+  presence: true
+
+  validates :movie,
+  presence: true
+
+  validates :text,
+  presence: true
+
+  validates :rating_out_of_ten,
+  numericality: { only_integer: true }
+  validates :rating_out_of_ten,
+  numericality: { greater_than_or_equal_to: 1 }
+  validates :rating_out_of_ten,
+  numericality: { less_than_or_equal_to: 10 }
+
 end
