91bd46489e6e05196346c6bbbe6e7f48a0e542a3
JulianOrinyol <julianorinyol@gmail.com>
2015-10-18 09:09:56 -0700

    fixed conditionals in cross origin action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -14,20 +14,11 @@ class AppointmentsController < ApplicationController
   end
 
   def handle_cor
-    binding.pry
-
     u = User.find_by_email(appointment_params[:email]) || User.create({:email => appointment_params[:email], :password => appointment_params[:email], :password_confirmation => appointment_params[:email], role: 3 })
     clinic_id = appointment_params[:clinic_id]
-    binding.pry
     if Appointment.where(user_id: u.id, clinic_id: clinic_id).empty? 
       a=Appointment.create(clinic_id: clinic_id, user_id: u.id)
     end
-    binding.pry
-
-
-    # u = User.create({:email => "guy@gmail.com", :password => "11111111", :password_confirmation => "11111111", role: 1 })
-    # name, email, password, phone_number, clinic_id
-
 
     result = "super success"
     render json: result
