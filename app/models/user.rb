c2f6a705372afe753e656f7806a42b882626a4f5
chiu <chiucode@gmail.com>
2015-10-18 03:05:57 +0000

    adjusted user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :vip, :admin, :patient, :clinic]
   # after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
